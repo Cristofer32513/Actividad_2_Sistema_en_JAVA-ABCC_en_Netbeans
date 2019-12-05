2dbe3c45f7551f69b99098ccbf93f512c7af12eb
Cristofer32513 <cristofer32513@gmail.com>
2019-12-05 14:57:20 -0600

    Se agrego la clase JPlaceholderPasswordField.

---
+++ src/main/java/modelo/JPlaceholderPasswordField.java
@@ -0,0 +1,48 @@
+package modelo;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JPasswordField;
+
+@SuppressWarnings("serial")
+public class JPlaceholderPasswordField extends JPasswordField {
+    private String ph;
+    Font fuenteArial18Normal=new Font("Arial", 0, 18);
+
+    public JPlaceholderPasswordField(String ph) {
+        this.ph = ph;
+    }
+    public JPlaceholderPasswordField() {
+        this.ph = null;
+    }
+
+    @Override
+    public String getText() {
+        String text = new String(super.getPassword());
+
+        if (text.trim().length() == 0 && ph != null) {
+            text = ph;
+        }
+
+        return text;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        String text = new String(super.getPassword());
+	
+        if (text.length() > 0 || ph == null) {
+            return;
+        }
+	
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(super.getDisabledTextColor());
+        g2.setFont(fuenteArial18Normal);
+        g2.drawString(ph, getInsets().left, g.getFontMetrics().getAscent() + getInsets().top);
+    }
+}
